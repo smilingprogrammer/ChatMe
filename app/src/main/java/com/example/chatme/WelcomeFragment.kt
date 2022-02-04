53cf874ff5ca3b49ac61ddab589e0b4d45a7557a
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-02-04 07:13:39 +0100

    First ChatMe

--- app/src/main/java/com/example/chatme/WelcomeFragment.kt
+++ app/src/main/java/com/example/chatme/WelcomeFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.chatme.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
-    private var _binding
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        _binding = FragmentWelcomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     companion object {
