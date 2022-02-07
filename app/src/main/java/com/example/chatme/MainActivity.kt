252b09c5fceebb06ec8b7c268cfd031a38a7f964
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-02-07 03:56:31 +0100

    Firebase Database Reference

--- app/src/main/java/com/example/chatme/MainActivity.kt
+++ app/src/main/java/com/example/chatme/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.chatme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.chatme.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,15 +17,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
 
         userList = ArrayList()
         adapter = UserAdapter(userList)
+
+        //binding userRecyclerview to the RecyclerView
+        userRecyclerView = binding.userRecyclerView
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        mDbRef.child("user").addChildEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (postSnapShot in snapshot.children){
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -30,6 +56,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
+        if(item.itemId ==  R.id.logout){
+            // The logout logic
+            mAuth.signOut()
+            val intent = Intent(this@MainActivity, LogIn::class.java)
+            finish()
+
+            return true
+        }
+        return true
     }
 }
